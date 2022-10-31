061bfc77b545af13b74ad13ee65dc87fc682fabc
Mayurp375 <mayurpolojwar1234@gmail.com>
2022-10-31 20:35:13 +0530

    main program

---
+++ employeeeWage.java
@@ -0,0 +1,15 @@
+public class employeeeWage {
+    public static void main(String[] args) {
+
+        System.out.println("welcome to employee wage");
+
+       // attendence attendence = new attendence();
+      //  attendence.Check();
+
+     //   dalyWage.dailyWage();
+     //   bySwitch.Switch();
+
+        //months.wageMonth();
+        dayOrMonths.month();
+    }
+}
